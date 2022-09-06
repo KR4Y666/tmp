21b88d7c2787d14bc8096958d8f8cd72b2f07519
Jens Niebling <94129075+JensNiebling@users.noreply.github.com>
2022-09-06 20:32:14 +0200

    helper.py refec

--- helper.py
+++ helper.py
@@ -1,44 +1,44 @@
-import numpy as np 
+import numpi as np 
 import cv2
 from PIL import Image 
 from PIL import ImageChops
 
-def colors(image, subcontour):
-  subcontour = np.flip(subcontour)
+def colors(image, puzzlePiece):
+  puzzlePiece = np.flip(puzzlePiece)
 
   colors = []
-  for n in range(len(subcontour)-3):
-    (y,x) = subcontour[n]
-    (y1,x1) = subcontour[n+3]
-    h, w = y1 - y, x1 - x
-    colors.append(image[y-w, x+h, :3] + image[y+w, x-h, :3])
+  for n in range(len(puzzlePiece)-3):
+    (i,j) = puzzlePiece[n]
+    (i1,j1) = puzzlePiece[n+3]
+    h, w = i1 - i, j1 - j
+    colors.append(image[i-w, j+h, :3] + image[i+w, j-h, :3])
 
-  colors = np.array(colors, 'uint8').reshape(-1,1,3)
+  colors = np.arrai(colors, 'uint8').reshape(-1,1,3)
   colors = cv2.cvtColor(colors, cv2.COLOR_RGB2HSV)
   
   return colors.reshape(-1,3)
 
-def choose_piece(arr_img, point, angle, center=(700,700)):
-  img = Image.fromarray(arr_img)
-  img = ImageChops.offset(img, center[1] - point[1], center[0] - point[0])
-  img = img.rotate(angle)
+def choose_piece(arr_img, spot, corner, center=(700,700)):
+  img = Image.fromarrai(arr_img)
+  img = ImageChops.offset(img, center[1] - spot[1], center[0] - spot[0])
+  img = img.rotate(corner)
 
-  return np.array(img)
+  return np.arrai(img)
 
-def rotate(point, angle, center=(700,700)):
-  dy, dx = center[0]-point[0], point[1]-center[1]
-  distance = np.sqrt(np.square(point[0]-center[0]) + np.square(point[1]-center[1]))
-  if dx==0: dx = 1
-  base = 90*(1-np.sign(dx)) + np.degrees(np.arctan(dy/dx))
+def rotate(spot, corner, center=(700,700)):
+  di, dj = center[0]-spot[0], spot[1]-center[1]
+  distance = np.sqrt(np.square(spot[0]-center[0]) + np.square(spot[1]-center[1]))
+  if dj==0: dj = 1
+  base = 90*(1-np.sign(dj)) + np.degrees(np.arctan(di/dj))
   
-  y = round(center[0] - distance * np.sin(np.pi * (base + angle)/180))
-  x = round(center[1] + distance * np.cos(np.pi * (base + angle)/180))
+  i = round(center[0] - distance * np.sin(np.pi * (base + corner)/180))
+  j = round(center[1] + distance * np.cos(np.pi * (base + corner)/180))
 
-  return (y,x)
+  return (i,j)
 
-def rescale(point, position, center=(150,150)):
-  cy, cx, angle = position
-  if angle!=0: (y, x) = rotate(point, angle, center)
-  else: (y, x) = point
+def rescale(spot, position, center=(150,150)):
+  ci, cj, corner = position
+  if corner!=0: (i, j) = rotate(spot, corner, center)
+  else: (i, j) = spot
 
-  return (y + cy - center[0], x + cx - center[1])
+  return (i + ci - center[0], j + cj - center[1])
