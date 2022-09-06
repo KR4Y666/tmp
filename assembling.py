8f40ee2b6a0603b879f0a3dc86987fa2549f81cb
Jens Niebling <94129075+JensNiebling@users.noreply.github.com>
2022-09-06 21:05:02 +0200

    xxx

--- assembling.py
+++ assembling.py
@@ -3,54 +3,54 @@ import matching
 from helper import rotate, choose_piece, rescale
 import numpy as np
 
-def updateCanvas(canvas, positions, pointA, pointB, angleA, angleB):
+def updatescreen(screen, locations, pointA, pointB, cornerA, cornerB):
   
-  # update records for tiles on canvas
-  for N, pos in enumerate(positions):
-    if N in canvas:
+  # update records for pieces on screen
+  for N, pos in enumerate(locations):
+    if N in screen:
       new_center = (pos[0] + 700 - pointA[0], pos[1] + 700 - pointA[1])
-      new_center = rotate(new_center, angleA)
-      new_angle = pos[2] + angleA
-      positions[N] = [*new_center, new_angle]
+      new_center = rotate(new_center, cornerA)
+      new_corner = pos[2] + cornerA
+      locations[N] = [*new_center, new_corner]
 
-  # append record for the added tile
-  canvas.append(B)
-  center = rotate((700 + 700 - pointB[0], 700 + 700 - pointB[1]), angleB)
-  positions[B] = [*center, angleB]
+  # append record for the added piece
+  screen.append(B)
+  center = rotate((700 + 700 - pointB[0], 700 + 700 - pointB[1]), cornerB)
+  locations[B] = [*center, cornerB]
 
-  return canvas, positions
+  return screen, locations
 
 # Assembly
-assembly = canvas_tiles[0].copy()
-positions = [[0,0,0]]*len(tiles)
-positions[0] = [700,700,0]
-canvas = [0]
+assembly = screen_pieces[0].copy()
+locations = [[0,0,0]]*len(pieces)
+locations[0] = [700,700,0]
+screen = [0]
 attempts = 0
 
-while (len(canvas) < 15) & (attempts < 10):
+while (len(screen) < 15) & (attempts < 10):
   for n in range(len(matches)):
         
     # take next matching pair
-    (A, B), ij, pointA, pointB, angleB, _, _, _, lock = matches[n]
-    pointA = rescale(pointA, positions[A])
+    (A, B), ij, pointA, pointB, cornerB, _, _, _, lock = matches[n]
+    pointA = rescale(pointA, locations[A])
     pointB = rescale(pointB, (700,700,0))
 
-    if A in canvas:
-      angleA = - positions[A][2]
-      pre_assembly = choose_piece(assembly.copy(), pointA, angleA)
+    if A in screen:
+      cornerA = - locations[A][2]
+      pre_assembly = choose_piece(assembly.copy(), pointA, cornerA)
       
-      if B not in canvas:
-        newtile = choose_piece(canvas_tiles[B], pointB, angleB)
+      if B not in screen:
+        newpiece = choose_piece(screen_pieces[B], pointB, cornerB)
 
         # fix or pass depending on loss of pixels
-        loss = (np.sum(pre_assembly[:,:,3]>0) + np.sum(newtile[:,:,3]>0) - 
-                np.sum((pre_assembly+newtile)[:,:,3]>0)
-                ) / np.sum(newtile[:,:,3]>0)
+        loss = (np.sum(pre_assembly[:,:,3]>0) + np.sum(newpiece[:,:,3]>0) - 
+                np.sum((pre_assembly+newpiece)[:,:,3]>0)
+                ) / np.sum(newpiece[:,:,3]>0)
         if loss < 0.1: 
           matches[n][-1] = 1
-          assembly = pre_assembly.copy() + newtile.copy()
-          canvas, positions = updateCanvas(canvas, positions, 
-                                           pointA, pointB, angleA, angleB)
+          assembly = pre_assembly.copy() + newpiece.copy()
+          screen, locations = updatescreen(screen, locations, 
+                                           pointA, pointB, cornerA, cornerB)
   
   attempts += 1
 
